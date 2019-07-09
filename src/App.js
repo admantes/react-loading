2f2ad9d9f8872a980aa5bb891ab9a40274596ee0
Aris Mantes <admantes@gmail.com>
2019-07-09 15:34:31 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Loading from './Loading';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isLoading, setLoading] = useState(true);
+	
+	
+	const finishLoading = ()=>{
+		setLoading(false);
+	}
+	
+	useEffect( ()=> {
+		
+		setTimeout( ()=>{
+			setLoading(false);
+		}, 5000 );
+	
+	});
+	
+	
+	if(isLoading){
+		 return (
+			<div>
+			<Loading /> 
+			<button onClick={finishLoading}> Finish Loading </button>
+		 </div>
+		  ); 
+	}else{
+		return (
+		<div> Loaded </div>
+		);
+	}
+	
+ 
 }
 
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import './App.css';
+
+function Loading() {
+  return (
+   <div className="lds-ripple"><div></div><div></div></div>
+  );
+}
+
+export default Loading;
